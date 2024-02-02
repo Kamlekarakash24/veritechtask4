d61cca21fe0bedf8bb923ba44ff8ed5a1114d053
Kamlekarakash24 <148519461+Kamlekarakash24@users.noreply.github.com>
2024-02-02 20:53:24 +0530

    Add files via upload

---
+++ moviesdata.js
@@ -0,0 +1,184 @@
+const movies=[
+    {"id":1,
+    "name":"Hanuman",
+    "year":"2024",
+    "lang":"telugu",
+    "poster":"images/hanuman.jpg"
+    },
+    {"id":2,
+    "name":"RRR",
+    "year":"2022",
+    "lang":"telugu",
+    "poster":"images/rrr.jpg"
+    },
+    {"id":3,
+    "name":"Pushpa:the rise",
+    "year":"2021",
+    "lang":"telugu",
+    "poster":"images/pushpa.jpg"
+    }
+    ,{"id":4,
+    "name":"Salaar",
+    "year":"2023",
+    "lang":"telugu",
+    "poster":"images/salaar.jpg"
+    }
+    ,{"id":5,
+    "name":"Guntur Kaaram",
+    "year":"2024",
+    "lang":"telugu",
+    "poster":"images/gunturkaram.jpg"
+    }
+    ,{"id":6,
+    "name":"Waltair Veerayya",
+    "year":"2023",
+    "lang":"telugu",
+    "poster":"images/waltairveeraya.jpg"
+    }
+    ,{"id":7,
+    "name":"Maharshi",
+    "year":"2019",
+    "lang":"telugu",
+    "poster":"images/maharshi.jpg"
+    }
+    ,{"id":8,
+    "name":"Attarintiki Daredi",
+    "year":"2013",
+    "lang":"telugu",
+    "poster":"images/attarinkidaredi.jpg"
+    }
+    ,{"id":9,
+    "name":"Saaho",
+    "year":"2019",
+    "lang":"telugu",
+    "poster":"images/saaho.jpg"
+    }
+    ,{"id":10,
+    "name":"Bahubali",
+    "year":"2015",
+    "lang":"telugu",
+    "poster":"images/bahubali.jpg"
+    }
+    ,{"id":11,
+    "name":"Fighter",
+    "year":"2024",
+    "lang":"hindi",
+    "poster":"images/fighter.jpg"
+    }
+    ,{"id":12,
+    "name":"12th Fail",
+    "year":"2023",
+    "lang":"hindi",
+    "poster":"images/12thfail.jpg"
+    }
+    ,{"id":13,
+    "name":"Animal",
+    "year":"2023",
+    "lang":"hindi",
+    "poster":"images/animal.jpg"
+    }
+    ,{"id":14,
+    "name":"Merry Christmas",
+    "year":"2024",
+    "lang":"hindi",
+    "poster":"images/merrychristmas.jpg"
+    }
+    ,{"id":15,
+    "name":"Dunki",
+    "year":"2023",
+    "lang":"hindi",
+    "poster":"images/dunki.jpg"
+    }
+    ,{"id":16,
+    "name":"Sam Bahadur",
+    "year":"2023",
+    "lang":"hindi",
+    "poster":"images/sambahadur.jpg"
+    }
+    ,{"id":17,
+    "name":"Tiger3",
+    "year":"2023",
+    "lang":"hindi",
+    "poster":"images/tiger3.jpg"
+    }
+    ,{"id":18,
+    "name":"Jawan",
+    "year":"2023",
+    "lang":"hindi",
+    "poster":"images/jawan.jpg"
+    }
+    ,{"id":19,
+    "name":"Main Atal Hoon",
+    "year":"2024",
+    "lang":"hindi",
+    "poster":"images/mainatalhoon.jpg"
+    }
+    ,{"id":20,
+    "name":"Pathaan",
+    "year":"2023",
+    "lang":"hindi",
+    "poster":"images/pataan.jpg"
+    }
+    ,{"id":21,
+    "name":"Shadow in the Cloud",
+    "year":"2020",
+    "lang":"english",
+    "poster":"images/shadowincloud.jpg"
+    }
+    ,{"id":22,
+    "name":"Pieces of a Woman",
+    "year":"2020",
+    "lang":"english",
+    "poster":"images/piecesofwoman.jpg"
+    }
+    ,{"id":23,
+    "name":"The White Tiger",
+    "year":"2021",
+    "lang":"english",
+    "poster":"images/whitetiger.jpg"
+    }
+    ,{"id":24,
+    "name":"The Little Things",
+    "year":"2021",
+    "lang":"english",
+    "poster":"images/littlethings.jpg"
+    }
+    ,{"id":25,
+    "name":"The Dig",
+    "year":"2021",
+    "lang":"english",
+    "poster":"images/dig.jpg"
+    }
+    ,{"id":26,
+    "name":"Our Friend",
+    "year":"2019",
+    "lang":"english",
+    "poster":"images/ourfriend.jpg"
+    }
+    ,{"id":27,
+    "name":"Raya and Last Dragon",
+    "year":"2021",
+    "lang":"english",
+    "poster":"images/lastdragon.jpg"
+    }
+    ,{"id":28,
+    "name":"Son",
+    "year":"2021",
+    "lang":"english",
+    "poster":"images/son.jpg"
+    }
+    ,{"id":29,
+    "name":"Godzilla vs. Kongs",
+    "year":"2021",
+    "lang":"english",
+    "poster":"images/godzillavskk.jpg"
+    }
+    ,{"id":30,
+    "name":"The Father",
+    "year":"2020",
+    "lang":"english",
+    "poster":"images/father.jpg"
+    }
+];
+
+localStorage.setItem("moviesdata",JSON.stringify(movies));
